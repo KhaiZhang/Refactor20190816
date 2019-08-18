911e594e156446c51540eb6bf9949bca349b2c07
Khai <KhaiZhang@163.com>
2019-08-19 02:39:20 +0800

    [feat:finish homework]

--- src/main/java/sales/SalesApp.java
+++ src/main/java/sales/SalesApp.java
@@ -6,61 +6,60 @@ import java.util.Date;
 import java.util.List;
 
 public class SalesApp {
+	SalesDao salesDao = new SalesDao();
+	SalesReportDao salesReportDao = new SalesReportDao();
+	EcmService ecmService = new EcmService();
 
 	public void generateSalesActivityReport(String salesId, int maxRow, boolean isNatTrade, boolean isSupervisor) {
-		
-		SalesDao salesDao = new SalesDao();
-		SalesReportDao salesReportDao = new SalesReportDao();
 		List<String> headers = null;
-		
-		List<SalesReportData> filteredReportDataList = new ArrayList<SalesReportData>();
-		
 		if (salesId == null) {
 			return;
 		}
 		
 		Sales sales = salesDao.getSalesBySalesId(salesId);
-		
 		Date today = new Date();
 		if (today.after(sales.getEffectiveTo())
 				|| today.before(sales.getEffectiveFrom())){
 			return;
 		}
-		
 		List<SalesReportData> reportDataList = salesReportDao.getReportData(sales);
+//		List<SalesReportData> filteredReportDataList = new ArrayList<SalesReportData>();
+//		for (SalesReportData data : reportDataList) {
+//			if ("SalesActivity".equalsIgnoreCase(data.getType())) {
+//				if (data.isConfidential()) {
+//					if (isSupervisor) {
+//						filteredReportDataList.add(data);
+//					}
+//				}else {
+//					filteredReportDataList.add(data);
+//				}
+//			}
+//		}
+//
+//		List<SalesReportData> tempList = new ArrayList<SalesReportData>();
+//		for (int i=0; i < reportDataList.size() || i < maxRow; i++) {
+//			tempList.add(reportDataList.get(i));
+//		}
+//		filteredReportDataList = tempList;
+
+		headers = getHeaders(isNatTrade);
+
+		SalesActivityReport report = this.generateReport(headers, reportDataList);
+		ecmService.uploadDocument(report.toXml());
 		
-		for (SalesReportData data : reportDataList) {
-			if ("SalesActivity".equalsIgnoreCase(data.getType())) {
-				if (data.isConfidential()) {
-					if (isSupervisor) {
-						filteredReportDataList.add(data);
-					}
-				}else {
-					filteredReportDataList.add(data);
-				}
-			}
-		}
-		
-		List<SalesReportData> tempList = new ArrayList<SalesReportData>();
-		for (int i=0; i < reportDataList.size() || i < maxRow; i++) {
-			tempList.add(reportDataList.get(i));
-		}
-		filteredReportDataList = tempList;
-		
+	}
+
+	protected List<String> getHeaders(boolean isNatTrade) {
+		List<String> headers;
 		if (isNatTrade) {
 			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Time");
 		} else {
 			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Local Time");
 		}
-		
-		SalesActivityReport report = this.generateReport(headers, reportDataList);
-		
-		EcmService ecmService = new EcmService();
-		ecmService.uploadDocument(report.toXml());
-		
+		return headers;
 	}
 
-	private SalesActivityReport generateReport(List<String> headers, List<SalesReportData> reportDataList) {
+	protected SalesActivityReport generateReport(List<String> headers, List<SalesReportData> reportDataList) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- src/test/java/sales/SalesAppTest.java
+++ src/test/java/sales/SalesAppTest.java
@@ -1,14 +1,116 @@
 package sales;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 public class SalesAppTest {
 
+//	@Test
+//	public void testGenerateReport() {
+//
+//		SalesApp salesApp = new SalesApp();
+//		salesApp.generateSalesActivityReport("DUMMY", 1000, false, false);
+//
+//	}
+
 	@Test
-	public void testGenerateReport() {
-		
-		SalesApp salesApp = new SalesApp();
-		salesApp.generateSalesActivityReport("DUMMY", 1000, false, false);
-		
+	public void testGenerateSalesActivityReport() {
+		SalesDao salesDao = mock(SalesDao.class);
+		SalesReportDao mockSalesReportDao = mock(SalesReportDao.class);
+		Sales spySales = spy(new Sales());
+		Date today = new Date();
+		ArrayList<SalesReportData> reportDataList = new ArrayList<>();
+		SalesReportData mockSalesReportData = mock(SalesReportData.class);
+		reportDataList.add(mockSalesReportData);
+		SalesActivityReport mockSalesActivityReport = mock(SalesActivityReport.class);
+		EcmService mockEcmService = mock(EcmService.class);
+		SalesApp salesApp = spy(new SalesApp());
+
+		when(salesDao.getSalesBySalesId(anyString())).thenReturn(spySales);
+		doReturn(new Date(today.getTime() - 86400000L)).when(spySales).getEffectiveFrom();
+		doReturn(new Date(today.getTime() + 86400000L)).when(spySales).getEffectiveTo();
+		when(mockSalesReportDao.getReportData(any())).thenReturn(reportDataList);
+		when(mockSalesReportData.getType()).thenReturn("SalesActivity");
+		doReturn(mockSalesActivityReport).when(salesApp).generateReport(anyList(),anyList());
+		when(mockSalesActivityReport.toXml()).thenReturn("XML REPORT");
+
+		ReflectionTestUtils.setField(salesApp,SalesApp.class,"salesDao",salesDao,SalesDao.class);
+		ReflectionTestUtils.setField(salesApp,SalesApp.class,"salesReportDao",mockSalesReportDao,SalesReportDao.class);
+		ReflectionTestUtils.setField(salesApp,SalesApp.class,"ecmService",mockEcmService,EcmService.class);
+
+		salesApp.generateSalesActivityReport("1",1,false,false);
+
+		verify(mockEcmService).uploadDocument(mockSalesActivityReport.toXml());
 	}
+
+	@Test
+	public void should_return_headers_contains_local_time_when_isNatTrade_is_false() {
+		boolean isNatTrade = false;
+		SalesApp salesApp = spy(SalesApp.class);
+		List<String> result = salesApp.getHeaders(isNatTrade);
+		Assert.assertTrue(result.get(3).contains("Local Time"));
+	}
+
+	@Test
+	public void should_return_headers_contains__time_when_isNatTrade_is_true() {
+		boolean isNatTrade = true;
+		SalesApp salesApp = spy(SalesApp.class);
+		List<String> result = salesApp.getHeaders(isNatTrade);
+		Assert.assertTrue(result.get(3).contains("Time"));
+	}
+
+	@Test
+	public void should_return_when_salesId_is_null() {
+		String salesId = null;
+		SalesApp salesApp = spy(SalesApp.class);
+		salesApp.generateSalesActivityReport(salesId,1,false,false);
+		SalesDao salesDao = mock(SalesDao.class);
+		ReflectionTestUtils.setField(salesApp,SalesApp.class,"salesDao",salesDao,SalesDao.class);
+		verify(salesDao,times(0)).getSalesBySalesId(salesId);
+	}
+
+	@Test
+	public void should_return_when_today_is_past_sales_Effective_to_date() {
+		String salesId = "1";
+		SalesDao salesDao = mock(SalesDao.class);
+		Sales spySales = spy(new Sales());
+		SalesApp salesApp = spy(SalesApp.class);
+		Date today = new Date();
+		ReflectionTestUtils.setField(salesApp,SalesApp.class,"salesDao",salesDao,SalesDao.class);
+
+		when(salesDao.getSalesBySalesId(anyString())).thenReturn(spySales);
+		doReturn(new Date(today.getTime() - 96400000L)).when(spySales).getEffectiveFrom();
+		doReturn(new Date(today.getTime() - 86400000L)).when(spySales).getEffectiveTo();
+		salesApp.generateSalesActivityReport(salesId,1,false,false);
+
+		verify(salesDao,times(1)).getSalesBySalesId(salesId);
+		verify(salesApp, times(0)).getHeaders(false);
+	}
+
+	@Test
+	public void should_return_when_today_is_before_sales_Effective_from_date() {
+		String salesId = "1";
+		SalesDao salesDao = mock(SalesDao.class);
+		Sales spySales = spy(new Sales());
+		SalesApp salesApp = spy(SalesApp.class);
+		Date today = new Date();
+		ReflectionTestUtils.setField(salesApp,SalesApp.class,"salesDao",salesDao,SalesDao.class);
+
+		when(salesDao.getSalesBySalesId(anyString())).thenReturn(spySales);
+		doReturn(new Date(today.getTime() + 86400000L)).when(spySales).getEffectiveFrom();
+		doReturn(new Date(today.getTime() + 96400000L)).when(spySales).getEffectiveTo();
+		salesApp.generateSalesActivityReport(salesId,1,false,false);
+
+		verify(salesDao,times(1)).getSalesBySalesId(salesId);
+		verify(salesApp, times(0)).getHeaders(false);
+	}
+
+
 }
